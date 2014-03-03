64bfec19c9bdd2e85d79a534f593331e653b69b1
coolBean <mrbean156@gmail.com>
2014-03-03 17:15:29 +0800

    更改‘需求稳定度’公式

--- module/indexcompare/control.php
+++ module/indexcompare/control.php
@@ -386,11 +386,8 @@ class indexcompare extends control
 		$productAdd = 0;
 		$productChange = 0;
 		for ($i=0; $i<count($temp); $i++){
-			if ($temp[$i]->initstory == 0) {
-				$temp[$i]->stability = '不存在';
-			} else {
-				$temp[$i]->stability = 100*round(($temp[$i]->addstory + $temp[$i]->changestory)/$temp[$i]->initstory, 4). '%'; 
-			}
+			$temp[$i]->stability = 100*round(1-
+						($temp[$i]->addstory+$temp[$i]->changestory)/($temp[$i]->addstory+$temp[$i]->changestory+$temp[$i]->initstory), 4). '%'; 
 			if ($temp[$i]->$key == $temp[$rowspanIndex]->$key) {
 				$productInit += $temp[$i]->initstory;
 				$productAdd += $temp[$i]->addstory;
@@ -399,7 +396,7 @@ class indexcompare extends control
 			} else {
 				$temp[$rowspanIndex]->rowspanVal = $rowspanValue;
 				$rowspanValue = 1;
-				$temp[$rowspanIndex]->productStability = 100*round(($productAdd+$productChange) / $productInit, 4). "%";
+				$temp[$rowspanIndex]->productStability = 100*round(1-($productAdd+$productChange) / ($productAdd+$productChange+$productInit), 4). "%";
 				
 				$rowspanIndex = $i;
 				$productInit = $temp[$i]->initstory;
@@ -409,7 +406,7 @@ class indexcompare extends control
 		}
 		if ($rowspanValue > 0) {
 			$temp[$rowspanIndex]->rowspanVal = $rowspanValue;
-			$temp[$rowspanIndex]->productStability = 100*round(($productAdd+$productChange) / $productInit, 4). "%";
+			$temp[$rowspanIndex]->productStability = 100*round(1-($productAdd+$productChange) / ($productAdd+$productChange+$productInit), 4). "%";
 		}
 		/* End. */
 		return $temp;
@@ -466,7 +463,6 @@ class indexcompare extends control
 					continue;
 				}
 				$sonArr = array();
-				$stabilityStr = '';
 				array_push($sonArr, $rs[0]);
 				array_push($sonArr, $rs[1]);
 				array_push($sonArr, $rs[2]);
@@ -477,11 +473,8 @@ class indexcompare extends control
 				if ($rs[5] == null) {
 					$rs[5] = 0;
 				}
-				if ($rs[3] == 0) {
-					$stabilityStr = '不存在';
-				} else {
-					$stabilityStr = 100*round(($rs[4] + $rs[5])/$rs[3], 4). '%';
-				}
+				
+				$stabilityStr = 100*round(1 - ($rs[4]+$rs[5])/$rs[3]+$rs[4]+$rs[5], 4). '%';
 					
 				array_push($sonArr, $rs[3]);
 				array_push($sonArr, $rs[4]);
